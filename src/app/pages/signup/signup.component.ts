4a9efcf4dd410b9034e47ca33eb423ce43504f43
IgnacioJuan <juan.guachichullca.est@tecazuay.edu.ec>
2023-04-25 17:22:59 -0500

    Initial commit

---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,48 @@
+import  Swal  from 'sweetalert2';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from './../../services/user.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  public usuario = {
+    username : '',
+    password : '',
+    rol:{'id_rol': 2}
+  }
+
+  constructor(private userService:UserService,private snack:MatSnackBar) { }
+
+  ngOnInit(): void {
+  }
+
+  formSubmit(){
+    console.log(this.usuario);
+    if(this.usuario.username == '' || this.usuario.username == null){
+      this.snack.open('El nombre de usuario es requerido !!','Aceptar',{
+        duration : 3000,
+        verticalPosition : 'top',
+        horizontalPosition : 'right'
+      });
+      return;
+    }
+
+    this.userService.añadirUsuario(this.usuario).subscribe(
+      (data) => {
+        console.log(data);
+        Swal.fire('Usuario guardado','Usuario registrado con exito en el sistema','success');
+      },(error) => {
+        console.log(error);
+        this.snack.open('Ha ocurrido un error en el sistema !!','Aceptar',{
+          duration : 3000
+        });
+      }
+    )
+  }
+
+}
