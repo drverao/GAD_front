7284b2bc012f51e4fe23e45378dbca156be8525e
dverao1997 <diego.r.vera.ortiz@gmail.com>
2023-05-08 23:15:07 -0500

    .

--- src/app/services/asignacion-indicador.service.ts
+++ src/app/services/asignacion-indicador.service.ts
@@ -2,8 +2,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { map, Observable, catchError } from "rxjs";
-import { AsignacionIndicador } from "../models/AsignacionIndicador";
 import baserUrl from './helper';
+import { AsignacionIndicador } from "../models/AsignacionIndicador";
 
 @Injectable({
     providedIn: 'root'
