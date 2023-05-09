4cc7877a41c0426dc35e3b2022338e5eba6d8e72
dverao1997 <diego.r.vera.ortiz@gmail.com>
2023-05-09 17:39:11 -0500

    cambios modelo
    se cambio el diseño del modelo

--- src/app/pages/superadmin/modelo/detalle-modelo/detalle-modelo.component.ts
+++ src/app/pages/superadmin/modelo/detalle-modelo/detalle-modelo.component.ts
@@ -8,6 +8,9 @@ import { Subcriterio } from 'src/app/models/Subcriterio';
 import { SubcriteriosService } from 'src/app/services/subcriterios.service';
 import { Indicador } from 'src/app/models/Indicador';
 import { IndicadoresService } from 'src/app/services/indicadores.service';
+import { ActivatedRoute } from '@angular/router';
+import { InicioModeloComponent } from '../inicio-modelo/inicio-modelo.component';
+
 @Component({
   selector: 'app-detalle-modelo',
   templateUrl: './detalle-modelo.component.html',
@@ -15,29 +18,61 @@ import { IndicadoresService } from 'src/app/services/indicadores.service';
 })
 export class DetalleModeloComponent implements OnInit {
 
-  listaGeneralModelo: number =0;
+  listaGeneralModelo: number = 0;
   modeloClase: Modelo[] = [];
-  criterioClase:Criterio[]=[];
-  subcriterioClase:Subcriterio[]=[];
-  indicadorClase:Indicador[]=[];
-  constructor(public modeloService: ModeloService ,public criterioService:CriteriosService, public subcriterioService:SubcriteriosService, public indicadorService:IndicadoresService, private router: Router){}
+  criterioClase: Criterio[] = [];
+  subcriterioClase: Subcriterio[] = [];
+  indicadorClase: Indicador[] = [];
+  componentm: InicioModeloComponent[] = [];
 
-  
+  datasource: any[] = [];
+
+  selectedCAD: Modelo | null = null;
+  elementoSeleccionado: any;
+  criterio: Criterio = new Criterio();
+  model: Modelo = new Modelo();
+  indicador: Indicador = new Indicador();
+  datosCargados = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    public modeloService: ModeloService,
+    public criterioService: CriteriosService,
+    public subcriterioService: SubcriteriosService,
+    public indicadorService: IndicadoresService,
+    private router: Router
+
+  ) { }
+
 
   ngOnInit(): void {
-   
+    this.listaModelo();
 
     this.criterioService.getCriterios().subscribe(data => {
       console.log(data);
     });
-    this.indicadorService.getIndicadors().subscribe(data => {
-      console.log(data);
-    });
 
-  
+
+    this.listaIndicador();
+
+    this.listaModelo();
+
+    if (!this.datosCargados) {
+      this.recibeModelo();
+    }
   }
 
-  
+
+
+
+
+
+
+  listaModelo() {
+    this.modeloService.listarModelo().subscribe(data => {
+      this.datasource = data;
+    });
+  }
   listaCriterio() {
     this.criterioService.listarCriterio()
       .subscribe(data => {
@@ -51,22 +86,43 @@ export class DetalleModeloComponent implements OnInit {
       })
   }
   listaIndicador() {
-    this.indicadorService.listarIndicador()
-      .subscribe(data => {
-        this.indicadorClase = data;
-      })
+
+
+
+    this.indicadorService.getIndicadors().subscribe(data => {
+      console.log(data);
+    });
   }
 
-  id:any='';
-  accordion(ids:any){
-    if(this.id==ids){
-      this.id='';
+  recibeModelo() {
+    let id = localStorage.getItem("id");
+    console.log(`El ID seleccionado es: ${id}`);
+    this.modeloService.getModeloById(Number(id))
+      .subscribe(data => {
+        this.model = data;
+        this.datosCargados = true;
+      });
+
+
+
+  }
+
+
+
+
+
+
+
+  id: any = '';
+  accordion(ids: any) {
+    if (this.id == ids) {
+      this.id = '';
     }
     else {
-      this.id=ids;
+      this.id = ids;
     }
 
   }
-  
+
 
 }
