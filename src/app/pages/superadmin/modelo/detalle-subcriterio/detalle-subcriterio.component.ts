e4fff7d2533152a8e001ce6181613f70c0e103fa
ABYGAIL1410 <72097225+ABYGAIL1905@users.noreply.github.com>
2023-05-10 00:20:46 -0500

    Revert "Revert "nuevos componentes""
    This reverts commit 92b673b4a10446a617370308be078f5a8b719424.

---
+++ src/app/pages/superadmin/modelo/detalle-subcriterio/detalle-subcriterio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-subcriterio',
+  templateUrl: './detalle-subcriterio.component.html',
+  styleUrls: ['./detalle-subcriterio.component.css']
+})
+export class DetalleSubcriterioComponent {
+
+}
