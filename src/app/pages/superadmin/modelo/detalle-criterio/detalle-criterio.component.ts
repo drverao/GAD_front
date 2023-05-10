228b2357c0c3c6f002a6d9dbd7104decfdc28fbd
ABYGAIL1410 <72097225+ABYGAIL1905@users.noreply.github.com>
2023-05-10 00:08:28 -0500

    Revert "."
    This reverts commit a6f715ec932923091bc7ab15ac151c3519433640.

---
+++ src/app/pages/superadmin/modelo/detalle-criterio/detalle-criterio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-criterio',
+  templateUrl: './detalle-criterio.component.html',
+  styleUrls: ['./detalle-criterio.component.css']
+})
+export class DetalleCriterioComponent {
+
+}
