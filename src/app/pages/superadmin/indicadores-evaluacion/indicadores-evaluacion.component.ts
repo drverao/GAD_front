0c81b64730ff782ff995b1ee893b97a9eca64ebd
IgnacioJuan <juan.guachichullca.est@tecazuay.edu.ec>
2023-05-09 20:11:12 -0500

    Conexion indicador - evaluacion

--- src/app/pages/superadmin/indicadores-evaluacion/indicadores-evaluacion.component.ts
+++ src/app/pages/superadmin/indicadores-evaluacion/indicadores-evaluacion.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Indicador } from 'src/app/models/Indicador';
 
 @Component({
   selector: 'app-indicadores-evaluacion',
   templateUrl: './indicadores-evaluacion.component.html',
   styleUrls: ['./indicadores-evaluacion.component.css']
 })
-export class IndicadoresEvaluacionComponent {
+export class IndicadoresEvaluacionComponent implements OnInit{
   textoMostrado = '';
   opcionSeleccionada = 'Ninguna opción seleccionada';
 
   mostrarH1(opcion: string) {
     this.textoMostrado = opcion;
     this.opcionSeleccionada = opcion;
+  }
+  indicador: Indicador = new Indicador();
+  ngOnInit() {
+    const data = history.state.data;
+    console.log(data); // aquí tendrías el objeto `indicador` de la fila seleccionada.
+    this.indicador = history.state.data;
   }
 }
