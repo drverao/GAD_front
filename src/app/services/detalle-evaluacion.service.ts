43e1a7780a3d2dec1fc5228d9084151bd5dbdb23
dianasaico20 <diana.saico.est@tecazuay.edu.ec>
2023-05-14 15:24:30 -0500

    .

--- src/app/services/detalle-evaluacion.service.ts
+++ src/app/services/detalle-evaluacion.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { detalleEvaluacion } from '../models/DetalleEvaluacion';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import baserUrl from './helper';
 
 @Injectable({
@@ -9,14 +9,15 @@ import baserUrl from './helper';
 })
 export class DetalleEvaluacionService {
 
-  evaluacionObj: detalleEvaluacion[] = [];
 
-  private httpHeaders= new HttpHeaders({'Content-Type':'application/json'})
   constructor(private http:HttpClient) { }
 
-
-  create(r:detalleEvaluacion):Observable<detalleEvaluacion>{
-    return this.http.post<detalleEvaluacion>( `${baserUrl}/api/detalle_evaluacion/crear`, r
+  create(r: detalleEvaluacion): Observable<detalleEvaluacion> {
+    return this.http.post<detalleEvaluacion>(`${baserUrl}/api/detalle_evaluacion/crear`, r).pipe(
+      catchError((error) => {
+        console.error(error);
+        throw error;
+      })
     );
   }
 
@@ -25,5 +26,20 @@ export class DetalleEvaluacionService {
    getDetalle():Observable<detalleEvaluacion[]>{
     return this.http.get<detalleEvaluacion[]>(`${baserUrl}/api/evidencia/listarRechazada`);
   }
+  /*
+  getDetalleEvi(idUsua: number, idEvi: number): Observable<detalleEvaluacion[]>{
+    return this.http.get<detalleEvaluacion[]>(`${baserUrl}/api/evidencia/listarporEviRecha/${idUsua}/${idEvi}`);
+  }*/
+
+  getDetalleEvi(idUsua: number, idEvi: number): Observable<detalleEvaluacion[]>{
+    return this.http.get<detalleEvaluacion[]>(`${baserUrl}/api/evidencia/listarporEviRecha/${idUsua}/${idEvi}`)
+      .pipe(
+        catchError(error => {
+          console.log('Error:', error);
+          return throwError('Hubo un error al obtener los detalles de evaluación');
+        })
+      );
+  }
+
   
 }
