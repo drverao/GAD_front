60157ec624780100bafd47c824887f23cc62f1c2
dianasaico20 <diana.saico.est@tecazuay.edu.ec>
2023-08-16 01:40:28 -0500

    Arreglo de estilos, add tabla

--- src/app/pages/admin/form-plan-operativo/form-plan-operativo.component.ts
+++ src/app/pages/admin/form-plan-operativo/form-plan-operativo.component.ts
@@ -8,26 +8,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./form-plan-operativo.component.css'],
   providers: [MessageService],
 })
+
+
+
 export class FormPlanOperativoComponent implements OnInit {
   items: MenuItem[];
   activeIndex: number = 0;
+  data: any[] = [];
+  column3Total: number = 0;
+  subcolumn1Total: number = 0;
+  subcolumn2Total: number = 0;
+  subcolumn3Total: number = 0;
+  subcolumn4Total: number = 0;
+  datatabla2: any[] = [];
 
-  constructor(
-    public messageService: MessageService,
-    private router: Router,
-  ) {
+  constructor(public messageService: MessageService,private router: Router,) {
     this.items = [];
   }
   ngOnInit(): void {
     this.items = [
       { label: 'Paso 1', command: (event: any) => this.changeStep(0) },
-      {
-        label: 'Paso 2',
-        command: (event: any) => this.changeStep(1),
-      },
+      {label: 'Paso 2', command: (event: any) => this.changeStep(1), },
       { label: 'Paso 3', command: (event: any) => this.changeStep(2) },
       { label: 'Paso 4', command: (event: any) => this.changeStep(3) },
-      { label: 'Confirmar', command: (event: any) => this.changeStep(4) },
+      { label: 'Paso 5', command: (event: any) => this.changeStep(4) },
     ];
   }
   changeStep(index: number) {
@@ -52,13 +56,51 @@ export class FormPlanOperativoComponent implements OnInit {
     this.activeIndex = index;
   }
   
- 
+
+removeRow(rowIndex: number): void {
+  this.data.splice(rowIndex, 1);
+  this.calculateTotalRemo(); 
+}
+
+  calculateTotal(rowData: any): void {
+    rowData.column4 = rowData.subcolumn1 + rowData.subcolumn2 + rowData.subcolumn3 + rowData.subcolumn4;
+    // Actualizar sumas individuales
+    this.column3Total = this.data.reduce((sum, row) => sum + row.column3, 0);
+    this.subcolumn1Total = this.data.reduce((sum, row) => sum + row.subcolumn1, 0);
+    this.subcolumn2Total = this.data.reduce((sum, row) => sum + row.subcolumn2, 0);
+    this.subcolumn3Total = this.data.reduce((sum, row) => sum + row.subcolumn3, 0);
+    this.subcolumn4Total = this.data.reduce((sum, row) => sum + row.subcolumn4, 0);
+  }
+
+
+calculateTotalRemo(): void {
+  this.column3Total = this.data.reduce((sum, row) => sum + row.column3, 0);
+  this.subcolumn1Total = this.data.reduce((sum, row) => sum + row.subcolumn1, 0);
+  this.subcolumn2Total = this.data.reduce((sum, row) => sum + row.subcolumn2, 0);
+  this.subcolumn3Total = this.data.reduce((sum, row) => sum + row.subcolumn3, 0);
+  this.subcolumn4Total = this.data.reduce((sum, row) => sum + row.subcolumn4, 0);
+}
+
+
+  addRow(): void {
+    this.data.push({
+      column1: '',
+      column2: '',
+      column3: 0,
+      subcolumn1: 0,
+      subcolumn2: 0,
+      subcolumn3: 0,
+      subcolumn4: 0,
+    });
+  }
+
+  calculateSumTotal(): number {
+    return this.data.reduce((sum, rowData) => sum + rowData.column4, 0);
+  }
   onActiveIndexChange(event: number) {
     this.activeIndex = event; }
 
-    save() {
-  
-    }
+    save() {}
     
   
     goBack() {
@@ -75,6 +117,7 @@ export class FormPlanOperativoComponent implements OnInit {
         this.changeStep(this.activeIndex + 1);}*/}
 
 
-        Cancelar() {
-          this.router.navigate(['/inicioModelo']); }
+ Cancelar() {
+        //  this.router.navigate(['/inicioModelo']);
+       }
 }
